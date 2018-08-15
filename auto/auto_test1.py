435d54c98e889c30e76f7c408ef8164666c061b7
VedWhat <ved.chitnis@gmail.com>
2018-08-15 12:07:17 +0530

    Ved Auto_Straight and GPS

--- auto/auto_test1.py
+++ auto/auto_test1.py
@@ -1,5 +1,5 @@
 import math,time,serial
-ser = serial.Serial(port='/dev/ttyUSB100',baudrate = 57600)
+ser = serial.Serial(port='/dev/ttyUSB50',baudrate = 57600)
 global obstacle_left, obstacle_right
 def straight():
 	stm_send='m4x4999y0000'
@@ -17,16 +17,22 @@ def backward():
 	stm_send='m4x4999y9999'	
 	print('Going backward')
 	ser.write(stm_send)
+def brute_stop():
+	stm_send='m4x4999y4999'
+	print('Brute Stop')	
 def obstacle_avoid():#TAKING DISTANCE IN CENTIMETERS
 	while True:
-		while(obstacle_right>100 and obstacle_left>100):
+		while(obstacle_right>50 and obstacle_left>50):
 			straight()
-		if (obstacle_right>100 and obstacle_left<100):
-			while(obstacle_left<100):
+		if (obstacle_right>50 and obstacle_left<50):
+			while(obstacle_left<50):
 				clockwise()	
-		if (obstacle_left>100 and obstacle_right<100):
-			while(obstacle_right<100):
+		if (obstacle_left>50 and obstacle_right<50):
+			while(obstacle_right<50):
 				anticlockwise()	
-		if(obstacle_right<100 and obstacle_left<100):
-			while(obstacle_left>100 or obstacle_right>100):
+		if(obstacle_right<50 and obstacle_left<50):
+			while(obstacle_left>50 or obstacle_right>50):
 				backward()
+		if(obstacle_right<25 and obstacle_left<25):
+			brute_stop()		
+			
